8511091f5ff46843600f09429c1ba7e0cbef87c4
Simone Tasca <simo.tasca@gmail.com>
2023-11-23 15:45:57 +0100

    tout le monde est debouger

--- packages/types/src/database/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./generated";
-import type { Database } from "./generated";
-
-export type Tables<T extends keyof Database["public"]["Tables"]> =
-  Database["public"]["Tables"][T]["Row"];
-export type Enums<T extends keyof Database["public"]["Enums"]> =
-  Database["public"]["Enums"][T];
-export type Functions<T extends keyof Database["public"]["Functions"]> =
-  Database["public"]["Functions"][T]["Returns"];
