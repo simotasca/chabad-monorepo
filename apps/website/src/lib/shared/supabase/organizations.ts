3f2d86d61e48e5e7bf15c60649431cad5f48dba7
Simone Tasca <simo.tasca@gmail.com>
2024-01-14 18:51:44 +0100

    contacts

--- apps/website/src/lib/shared/supabase/organizations.ts
+++ apps/website/src/lib/shared/supabase/organizations.ts
@@ -1,9 +1,16 @@
+import type { Tables } from "@/db-types";
 import type { WithSlug } from "../routes";
 import routes from "../routes";
+import { splitContacts } from "./contacts";
 
 /**
  * maps the new's **slug** to the corresponding **Astro url**
  */
 export function organizationsMapper<T extends WithSlug>(o: T) {
   return { ...o, url: routes.organization(o) };
+}
+
+export function organizationContactsMapper<T extends { contacts: Tables<"contacts">[] }>(o: T) {
+  const { contacts, ...organization } = o;
+  return { ...organization, ...splitContacts(contacts) };
 }
