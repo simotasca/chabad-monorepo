7fdfabef464f111df274c4e4835bdaa2ff528e80
emanuele-hazan <emanuele.hazan@gmail.com>
2024-02-02 17:10:55 +0100

    ne ho 11

--- apps/website/src/lib/server/mdx.ts
+++ apps/website/src/lib/server/mdx.ts
@@ -2,7 +2,7 @@ import * as runtime from "react/jsx-runtime"; // Production.
 import { compile, run } from "@mdx-js/mdx";
 
 export async function parseMdx(mdxContent?: string | null) {
-  if (!mdxContent) return null;
+  if (!mdxContent) return "div";
 
   const code = String(
     await compile(mdxContent, {
--- apps/website/src/lib/shared/supabase/live.ts
+++ apps/website/src/lib/shared/supabase/live.ts
@@ -1,14 +1,10 @@
 import type { Tables } from "@/db-types";
 
-type WithSlug = Pick<Tables<"live_video">, "slug">;
-
-export function liveUrl({ slug }: WithSlug) {
-  return `/video-live/${slug}`;
-}
+type WithSlug = Pick<Tables<"live_video">, "youtube_link">;
 
 /**
  * maps the new's **slug** to the corresponding **Astro url**
  */
 export function livesMapper<T extends WithSlug>(l: T) {
-  return { ...l, url: liveUrl(l) };
+  return { ...l };
 }
